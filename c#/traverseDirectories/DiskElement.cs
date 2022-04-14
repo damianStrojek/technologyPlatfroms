bfd185cd59bdd9a0c1d983ae588fe5238f59feb8
damianFresh <67586060+damianStrojek@users.noreply.github.com>
2022-04-14 20:24:09 +0200

    Add files via upload

---
+++ c#/traverseDirectories/DiskElement.cs
@@ -0,0 +1,18 @@
+﻿using System.IO;
+
+namespace Lab_1 {
+	public abstract class DiskElement {
+		protected FileSystemInfo file;
+
+		protected internal abstract void Print(int depth);
+		protected abstract string Format(int depth);
+
+		public FileSystemInfo File{
+			get {
+				return file;
+			}
+		}
+
+		public void Print() { Print(0); }
+	}
+}
---
+++ c#/traverseDirectories/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+
+namespace Lab_1{
+	class Program{
+		static void Main(string[] Args){
+			if (Args.Length < 1){
+				Console.Error.WriteLine("Error: no input path given.");
+				return;
+			}
+			else if (Directory.Exists(Args[0]) == false){
+				Console.Error.WriteLine("Error: path incorrect.");
+				return;
+			}
+
+			DiskDirectory dd = new DiskDirectory(new DirectoryInfo(Args[0]));
+			dd.Print();
+
+			Console.WriteLine("Input enter to terminate");
+			Console.ReadLine();
+		}
+	}
+}
